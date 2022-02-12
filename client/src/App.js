0b55a049b6f72a55d24d09591769de7551dbf78d
Egbor <egorminsk2001@gmail.com>
2022-02-12 20:21:12 +0300

    Added template of SignIn page

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import { Button, Row, Col } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <Row className="mx-0">
+        <Button as={Col} variant="primary">Button #1</Button>
+        <Button as={Col} variant="secondary" className="mx-2">Button #2</Button>
+        <Button as={Col} variant="success">Button #3</Button>
+      </Row>
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +23,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
---
+++ client/src/js/WCSignIn.js
@@ -0,0 +1,38 @@
+import { Form } from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
+
+import '../css/WCCommon.css';
+import '../css/WCAuthorization.css';
+
+function WCSignIn() {
+    return(
+        <div className="wcauth-wrapper">
+            <div className="wcauth-title-wrapper">
+                <div className="wc-fw-bold wcauth-title-text">WebChat</div>
+            </div>
+            <div className="mb-3">
+                <Form>
+                    <Form.Group className="mb-3">
+                        <Form.Control placeholder="Login"/>
+                    </Form.Group>
+                    <Form.Group className="mb-3">
+                        <Form.Control type="password" placeholder="Password"/>
+                    </Form.Group>
+                </Form>
+                <div className="d-grid">
+                    <Button  type="submit">Sign in</Button>
+                </div>
+            </div>
+            <div className="wcauth-split-wrapper">
+                <div className="wcauth-split-line"></div>
+                <div className="wcauth-split-text">or</div>
+                 <div className="wcauth-split-line"></div>
+            </div>
+            <div className="wcauth-alt-wrapper">
+                <a className="wcauth-alt-link" href="#">Sign up</a>
+            </div>
+        </div>
+    );
+}
+
+export default WCSignIn;
